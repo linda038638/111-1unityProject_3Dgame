717a1ee999786095a1557025a0e73c8a7be6ee82
Snow.fishb <72215312+linda038638@users.noreply.github.com>
2023-01-05 02:56:38 +0800

    門軸心

--- 3Dgame_UnityProject/Assets/Scripts/InteractableSystem.cs
+++ 3Dgame_UnityProject/Assets/Scripts/InteractableSystem.cs
@@ -10,30 +10,45 @@ namespace Misun
     /// </summary>
     public class InteractableSystem : MonoBehaviour
     {
+
+       
         [SerializeField, Header("對話資料")]
         private DialogueData dataDialogue;
         [SerializeField, Header("對話結束後的事件")]
         private UnityEvent onDiaogueFinish;
-
+        [SerializeField, Header("啟動道具")]
+        private GameObject propActive;
+        [SerializeField, Header("啟動後對話資料")]
+        private DialogueData dataDialogueActive;
         private string nameTarget = "PlayerCapsule";
 
         private DialogueSystem dialogueSystem;
         private void Awake()
         {
             dialogueSystem = GameObject.Find("畫布對話系統").GetComponent<DialogueSystem>();
+        
         }
 
         private void OnTriggerEnter(Collider other)
         {
             if(other.gameObject.name == nameTarget)
             {
-                dialogueSystem.StartDialogue(dataDialogue);
+                if(propActive == null || propActive.activeInHierarchy)
+                {
+                    dialogueSystem.StartDialogue(dataDialogue, onDiaogueFinish);
+                }
+                else
+                {
+                    dialogueSystem.StartDialogue(dataDialogueActive);
+                }
+                
             }
         }
 
         public void HiddenObject()
         {
             gameObject.SetActive(false);
+            Debug.Log("do!");
         }
 
     }
